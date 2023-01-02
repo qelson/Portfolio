4098a1fef606686273e6c8ecd056b9763b8ea183
Quin A <qelson@protonmail.com>
2023-01-02 18:14:14 -0500

    fixed some stuff

---
+++ script.js
@@ -0,0 +1,15 @@
+function scrollToHome() {
+    document.getElementById('home').scrollIntoView();
+}
+
+function scrollToAbout() {
+    document.getElementById('about').scrollIntoView();
+}
+
+function scrollToProjects() {
+    document.getElementById('projects').scrollIntoView();
+}
+
+function scrollToContact() {
+    document.getElementById('contact').scrollIntoView();
+}
